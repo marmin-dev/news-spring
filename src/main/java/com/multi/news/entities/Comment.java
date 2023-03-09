e93aa9131b715fea7f331fd853dc794bdddd98e5
Marmin <marmin.dev@gmail.com>
2023-03-09 15:27:06 +0900

    Add Entity and Repository, Unit test success

---
+++ src/main/java/com/multi/news/entities/Comment.java
@@ -0,0 +1,29 @@
+package com.multi.news.entities;
+
+import com.sun.istack.NotNull;
+import lombok.*;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Table(name = "Comment")
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private String author;
+
+
+}
